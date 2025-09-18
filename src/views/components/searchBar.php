aebad1bc4c8c76db3f7eee7e26e8be7ee4d78950
Ismael . <ismaelmolina856@gmail.com>
2025-09-18 11:40:48 -0300

    Clients - searchBar

--- src/views/components/searchBar.php
+++ src/views/components/searchBar.php
@@ -1,5 +1,5 @@
 <div>
-    <div class="p-4 border-b border-gray-200 flex items-center space-x-4">
+    <div class="flex items-center space-x-4">
         <div class="flex-grow">
             <input type="text" placeholder="Buscar miembro por nombre" class="w-full px-3 py-2 border border-gray-300 rounded-md">
         </div>
--- src/views/pages/clients.view.php
+++ src/views/pages/clients.view.php
@@ -7,8 +7,22 @@
     <main class="flex-grow flex flex-col">
 
         <div class="flex flex-grow overflow-hidden">
-            
-            <?php include __DIR__ . '/../components/clientList.php'; ?>
+
+            <div class="w-1/4 h-full flex flex-col border-r border-gray-200">
+                <div class="flex-shrink-0 p-4 bg-white border-b border-gray-200">
+                    <div class="flex items-center space-x-2">
+                        <div class="flex-grow">
+                            <?php include __DIR__ . '/../components/searchBar.php'; ?>
+                        </div>
+                        <div class="flex-shrink-0">
+                            <?php include __DIR__ . '/../components/clients/btnAddClient.php'; ?>
+                        </div>
+                    </div>
+                </div>
+                <div class="flex-grow overflow-y-auto">
+                    <?php include __DIR__ . '/../components/clientList.php'; ?>
+                </div>
+            </div>
 
             <div class="w-3/4 p-6 flex flex-col space-y-6 overflow-y-auto">
                 <div class="grid grid-cols-1 md:grid-cols-2 gap-6">
