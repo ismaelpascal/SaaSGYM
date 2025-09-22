cd665f6eb218dbdca53946c79581a8b133928642
Ismael . <ismaelmolina856@gmail.com>
2025-09-21 21:24:16 -0300

    phinx

--- db/seeds/TestMemberSeeder.php
+++ db/seeds/TestMemberSeeder.php
@@ -8,13 +8,14 @@ class TestMemberSeeder extends AbstractSeed
 {
     public function run(): void
     {
-        $data = [
-            [
-                'nombre' => 'Juan',
-                'apellido' => 'Perez',
-                'email' => 'juan.perez@example.com'
-            ]
-        ];
+        $data = [];
+        for ($i = 1; $i <= 100; $i++) {
+            $data[] = [
+                'nombre'   => 'Socio de Prueba',
+                'apellido' => $i,
+                'email'    => 'socio' . $i . '@example.com',
+            ];
+        }
 
         $this->table('members')->insert($data)->saveData();
     }
