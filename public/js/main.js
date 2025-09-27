7718dcf9c16bfe82f5e2c59e77a6413de153ed69
Ismael . <ismaelmolina856@gmail.com>
2025-09-27 18:11:22 -0300

    JS

--- public/js/main.js
+++ public/js/main.js
@@ -1,9 +1,9 @@
-import initAddClients from './clients/addClients.js';
-import initAddNotes from './clients/addNotes.js';
-import initAttendanceHistory from './clients/attendanceHistory.js';
-import initSearchFilter from './clients/addPayment.js';
+import initAddClients from './components/clients/addClients.js';
+import initAddNotes from './components/clients/addNotes.js';
+import initAttendanceHistory from './components/clients/attendanceHistory.js';
+import initSearchFilter from './components/clients/addPayment.js';
 
-import initAddPlans from './plans/addPlans.js';
+import initAddPlans from './components/plans/addPlans.js';
 
 initAddNotes();
 initAddClients();
